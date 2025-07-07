fee9fe44a74da2549fb74e5e8d31b6a66d48ca7b
Adhdhyan Jain <adhdhyan.jain@gmail.com>
2025-07-07 11:00:54 +0530

    remo

--- minimal_build/airtable.js
+++
@@ -1,9 +0,0 @@
-const Airtable = require('airtable');
-
-// Replace with your real values
-const base = new Airtable({ apiKey: 'patRt81WjTZzm9GOF.66aaff18dcc849e34dcd119ff6082cd1cd063869a1b09cf855db0ee1fd645cde' }).base('appcyyyoDv1ymsibp');
-
-const usersTable = base('MinimalTestBase');
-
-module.exports = { usersTable };
- 
